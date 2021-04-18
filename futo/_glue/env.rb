66b0ed9487f53c7e35e280dd74c545acbe9139ec
Sean Felipe <sean@addlightness.tech>
2021-04-17 23:22:57 -0700

    futo goodness, env.rb allow with or without leading /

--- futo/_glue/env.rb
+++ futo/_glue/env.rb
@@ -5,10 +5,12 @@ require 'json'
 HOST = 'http://localhost:3000'
 
 def hget(ep)
+  ep = '/' + ep unless ep.start_with? '/'
   HTTParty.get("#{HOST}#{ep}")
 end
 
 def hpost(ep, bb)
+  ep = '/' + ep unless ep.start_with? '/'
   hh = { 'Content-Type' => 'application/json' }
   url = "#{HOST}#{ep}"
   HTTParty.post(url, headers: hh, body: bb.to_json)
