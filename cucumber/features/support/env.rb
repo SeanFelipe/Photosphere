accb2d007fe57ead60ba40e7c03718007abd6bf4
Sean Felipe <sean@addlightness.tech>
2021-04-20 13:15:49 -0700

    post run record with details, cull Before hook/After hook from steps

--- cucumber/features/support/env.rb
+++ cucumber/features/support/env.rb
@@ -1,18 +1,26 @@
 require 'httparty'
 require 'byebug'; alias :breakpoint :byebug
+alias :bp :byebug
 require 'selenium-webdriver'
+
+class SpecNotFoundError < RuntimeError; end
 
 HOST = 'http://localhost:9292'
 
-def post_new_spec(summary, details)
-  ep = '/specs'
+def hpost(ep, body)
+  ep = '/' + ep unless ep.start_with? '/'
   url = HOST + ep
   headers = { 'Content-Type' => 'application/json' }
+  HTTParty.post(url, headers: headers, body: body.to_json)
+end
+
+def post_new_spec(summary, details)
+  ep = '/specs'
   body = {
     summary: summary,
     description: details
-  }.to_json
-  HTTParty.post(url, headers: headers, body: body)
+  }
+  hpost(ep, body)
 end
 
 def post_scenario_to_photosphere(scenario)
@@ -22,3 +30,36 @@ def post_scenario_to_photosphere(scenario)
   post_new_spec(title, steps)
 end
 
+def get_spec_id_from_photosphere_by_scenario_name(scenario)
+  ep = '/search'
+  body = { summary: scenario.name }
+  r = hpost(ep, body).parsed_response
+  if r['spec_id'] == 'not found'
+    raise SpecNotFoundError, "Could not find a Photosphere spec for scenario \"#{scenario.name}\""
+  end
+  return r['spec_id']
+end
+
+def post_test_run_record_to_photosphere(scenario)
+  iid = get_spec_id_from_photosphere_by_scenario_name(scenario)
+  ep = '/run_records'
+  run_result = scenario.passed? ? 'pass' : 'fail'
+  culled_steps = []
+  cull_values = ['Before hook', 'After hook']
+  scenario.test_steps.each do |ts|
+    add = true
+    cull_values.each do |cull_value|
+      if ts.text.start_with? cull_value
+        add = false
+      end
+    end
+    culled_steps << ts if add
+  end
+  details = culled_steps.collect{|ss| ss.text}.join("\n")
+  body = {
+    spec_id: iid,
+    result: run_result,
+    details: details,
+  }
+  hpost(ep, body)
+end
--- cucumber/features/support/hooks.rb
+++ cucumber/features/support/hooks.rb
@@ -1,6 +1,12 @@
 Before do |scenario|
-  if ENV['PHOTOSPHERE_POST'] == 'true'
+  if ENV['PHOTOSPHERE_POST_SPECS_FROM_CUCUMBER'] == 'true'
     post_scenario_to_photosphere scenario
     skip_this_scenario
+  end
+end
+
+After do |scenario|
+  unless ENV['PHOTOSPHERE_POST_SPECS_FROM_CUCUMBER'] == 'true'
+    post_test_run_record_to_photosphere(scenario)
   end
 end
